3fd54006b7eb3f769f53fd93f1203981e8ca93b4
matt <vaporlinx@gmail.com>
2017-06-18 11:08:12 -0600

    clean up dragging some

--- src/components/componentInventory.js
+++ src/components/componentInventory.js
@@ -6,7 +6,18 @@ import * as templateHelper from "../helpers/template.js";
       :host {
         display: flex;
         flex-direction: column;
-        width: 210px;
+      }
+
+      #inventory {
+        background:rgba(255,255,255,0.2); /* fallback */
+        background:
+          linear-gradient(135deg, transparent 5px,rgba(255,255,255,0.2) 0) top left,
+          linear-gradient(225deg, transparent 5px,rgba(255,255,255,0.2) 0) top right,
+          linear-gradient(315deg, transparent 5px,rgba(255,255,255,0.2) 0) bottom right,
+          linear-gradient(45deg,  transparent 5px,rgba(255,255,255,0.2) 0) bottom left;
+        background-size: 50% 50%;
+        background-repeat: no-repeat;
+        padding: 5px;
       }
 
       .hidden {
@@ -51,27 +62,50 @@ import * as templateHelper from "../helpers/template.js";
           event.dataTransfer.setData("text/plain", JSON.stringify(dragData));
           this.dragging = true;
         },
-        handleDrop: event => {
-          this.inventory = this.inventory.concat(JSON.parse(event.dataTransfer.getData("text/plain")));
-          event.stopPropagation();
-        },
-        handleDragEnter: event => {
-          if (!this.dragging) {
-            event.preventDefault();
-            event.dataTransfer.dropEffect = "move";
-          }
-        },
-        handleDragOver: event => {
-          if (!this.dragging) {
-            event.preventDefault();
-          }
-        },
+        // handleDrop: event => {
+        //   this.inventory = this.inventory.concat(JSON.parse(event.dataTransfer.getData("text/plain")));
+        //   event.stopPropagation();
+        // },
+        // handleDragEnter: event => {
+        //   if (!this.dragging) {
+        //     event.preventDefault();
+        //     event.dataTransfer.dropEffect = "move";
+        //   }
+        // },
+        // handleDragOver: event => {
+        //   if (!this.dragging) {
+        //     event.preventDefault();
+        //   }
+        // },
         handleDragEnd: (event, oldIndex) => {
           if (event.dataTransfer.dropEffect === "move") {
             this.inventory = this.inventory.filter((item, index) => index !== oldIndex);
           }
         },
       }];
+      this.inventoryElem.addEventListener("dragover", event => {
+        console.log("dragover");
+        if (!this.dragging) {
+          event.preventDefault();
+        }
+      }, false);
+      this.inventoryElem.addEventListener("dragenter", event => {
+        console.log("dragenter");
+        if (!this.dragging) {
+          event.preventDefault();
+          event.dataTransfer.dropEffect = "move";
+        }
+      }, false);
+      this.inventoryElem.addEventListener("drop", event => {
+        console.log("drop");
+        this.inventory = this.inventory.concat(JSON.parse(event.dataTransfer.getData("text/plain")));
+        event.stopPropagation();
+      }, false);
+      this.inventoryElem.addEventListener("dragend", event => {
+        if (this.dragging) {
+          this.dragging = false;
+        }
+      }, false);
     }
 
     get inventory() {
